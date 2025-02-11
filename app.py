43b234325fb917bf535d598f386d23448ce2d709
VincyLi-Li <li_wenxin023@163.com>
2025-02-11 04:56:53 +0000

    1

--- app.py
+++ app.py
@@ -7,5 +7,4 @@ def home():
     return render_template("index.html")  # ✅ Flask 自动在 templates/ 目录查找
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    app.run(debug=True, port=5001)  # 运行在5001端口
